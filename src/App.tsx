47794c0bd3fcc48de84a11e241307625bbbc8a67
Yehor <chaliyegor00@gmail.com>
2025-11-19 17:33:14 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Cocktails from './components/Cocktails'
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+export default function App() {
+	return (
+		<main>
+			<Navbar />
+			<Hero />
+			<Cocktails />
+		</main>
+	)
+}
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import { qrcode } from 'vite-plugin-qrcode'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), qrcode()],
+  base: '/gsap-first-project-ghPages'
+})
